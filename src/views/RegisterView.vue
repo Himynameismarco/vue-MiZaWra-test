926fdbf32bccc6a9ee654499d4b989e6390bf3d0
Himynameismarco <marcozander94@web.de>
2023-06-14 16:37:50 +0200

    added background to register page

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,38 +3,61 @@ import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
 
 </script>
 <template>
-  <NavBarLoggedOut />
-  <div class="register-content">
-    <div class="heading">
-      <h1 class="heading-logged-out">Create an Account</h1>
-      <h3 class="heading-logged-out">Already have an Account? <a>Log In</a></h3>
-    </div>
-    <form class="register-form" action="register" method="post">
-      <label for="firstName">First name</label>
-      <input type="text" id="firstName" name="firstName">
-      <label for="lastName">Last name</label>
-      <input type="text" id="lastName" name="lastName">
-      <label for="email">E-Mail</label>
-      <input type="email" id="email" name="email">
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password">
-      <p class="sub-password">(Use eight or more characters with a mix of letters, numbers, and symbols)</p>
-    </form>
+  <div class="background">
   </div>
+  <NavBarLoggedOut />
+    <div class="register">
+      <div class="content">
+        <div class="heading">
+          <h1 class="heading-logged-out">Create an Account</h1>
+          <h3 class="heading-logged-out">Already have an Account? <a>Log In</a></h3>
+        </div>
+        <form class="register-form" action="register" method="post">
+          <label for="firstName">First name</label>
+          <input type="text" id="firstName" name="firstName">
+          <label for="lastName">Last name</label>
+          <input type="text" id="lastName" name="lastName">
+          <label for="email">E-Mail</label>
+          <input type="email" id="email" name="email">
+          <label for="password">Password</label>
+          <input type="password" id="password" name="password">
+          <p class="sub-password">(Use eight or more characters with a mix of letters, numbers, and symbols)</p>
+          <button class="sign-up">Sign Up</button>
+        </form>
+      </div>
+    </div>
 </template>
 
 
 <style scoped>
 
-.register-content {
-  grid-area: 3 / 2 / 5 / 4;
+
+.register {
+  position: relative;
+  display: grid;
+  grid-template-columns: auto 462px auto; /* 5 */
+  grid-template-rows: 229px auto; /* 6 */
+}
+
+.background {
+  left: -949px;
+  top: calc(var(--nav-height) + 446px);
+  height: 1684px;
+  width: 1517px;
+  position: absolute;
+  background: radial-gradient(50% 50% at 50% 50%, rgba(205, 232, 43, 0.5) 0%, rgba(205, 232, 43, 0) 100%);
+}
+
+.register .content {
+  position: relative;
+  grid-area: 2 / 2 / 3 / 3;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   width: 100%;
 }
 
-.register-content .heading {
+.content .heading {
   flex-direction: column;
   justify-content: flex-start;
 }
@@ -44,6 +67,7 @@ import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+  margin-bottom: 287px;
 }
 
 #password {
@@ -54,15 +78,18 @@ import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
   font-size: var(--font-size-label);
   font-family: "Roboto Flex", Roboto Monospaced, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
 }
-@media (max-width: 1024px) {
-  .register-content {
-    grid-area: 2 / 2 / 5 / 5;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    width: 100%;
-  }
+
+.sign-up {
+  background: var(--white);
+  margin-top: 60px;
+
+  height: 45px;
+  border: 1px solid rgba(102, 102, 102, 0.1);
+  box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 30px;
 }
+
+
 
 </style>
 
