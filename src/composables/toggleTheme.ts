e154fc463c735c8d885338d6b6360baa8a0515e2
Himynameismarco <marcozander94@web.de>
2023-08-14 19:49:05 +0200

    deleting not used code

--- src/composables/toggleTheme.ts
+++ src/composables/toggleTheme.ts
@@ -1,32 +1,6 @@
 import {reactive, ref} from "vue";
 
 const theme = 'dark-theme';
-/*export default {
-  setup() {
-    const dark = ref(false);
-
-    function manipulateClass() {
-      console.log("dark in manipulateClass: " + dark.value);
-      if (dark.value) {
-        document.documentElement.className = theme;
-      } else {
-        document.documentElement.classList.remove(theme);
-      }
-    }
-
-    function toggleTheme() {
-      dark.value = !dark.value;
-      console.log("dark in toggleTheme: " + dark.value);
-      manipulateClass();
-    }
-
-    return {
-      dark,
-      toggleTheme
-    }
-  }
-};*/
-
 
 export const themeSwitch = reactive({
   dark: false,
