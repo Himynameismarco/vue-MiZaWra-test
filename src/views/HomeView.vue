925f617f5d3dbc611c8b335e38f5add71507fccc
Himynameismarco <marcozander94@web.de>
2023-10-28 14:18:16 +0200

    All submodes lead to same writing page now

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -116,7 +116,7 @@ let createDestroy = createOrDestroy();
         <h3>Choose Between Three Writing Modes</h3>
       </div>
       <div class="mode-picker">
-      <RouterLink class="no-background" to="/freeWriting">
+      <RouterLink class="no-background" :to="{ path: '/writing', query: { mode: 'FREE' } }">
         <div @mouseenter="hover = true; changeBackground($event);" @mouseleave="changeBackground($event);" class="mode" id="free">
           <svg class="icon" width="56" height="56" viewBox="0 0 70 70" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path id="free-svg" fill-rule="evenodd" clip-rule="evenodd" d="M35 70C54.33 70 70 54.33 70 35C70 15.67 54.33 0 35 0C15.67 0 0 15.67 0 35C0 54.33 15.67 70 35 70ZM35 49C42.732 49 49 42.732 49 35C49 27.268 42.732 21 35 21C27.268 21 21 27.268 21 35C21 42.732 27.268 49 35 49Z" fill="#3060FF"/>
