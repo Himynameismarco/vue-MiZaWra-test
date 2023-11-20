e8ae9a6eaf637066491da8dc480447b4a0d1905c
Himynameismarco <marcozander94@web.de>
2023-11-20 16:41:07 +0100

    Added Password Validation to Forget Password and Registration

--- src/views/ForgotPasswordNewPassword.vue
+++ src/views/ForgotPasswordNewPassword.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
+import { usePasswordValidation } from '../composables/usePasswordValidation';
+
+const { password, passwordConfirmation, passwordError, validatePassword } = usePasswordValidation();
+
+function handleSubmit() {
+  if (validatePassword()) {
+    // Continue with password reset logic
+    // TODO: Password reset logic goes here
+  }
+}
 
 </script>
 
@@ -11,11 +21,12 @@ import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
         <h1 class="heading-logged-out">Set New Password</h1>
         <h3 class="heading-logged-out">It must be at least 8 characters. </h3>
       </div>
-      <form class="forms" @submit="tbd"> <!-- TODO -->
+      <form class="forms" @submit.prevent="handleSubmit"> <!-- TODO @submit -->
         <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+        <input type="password" id="password" name="password" v-model="password">
         <label for="password">Password Confirmation</label>
-        <input type="password" id="passwordConfirmation" name="passwordConfirmation">
+        <input type="password" id="passwordConfirmation" name="passwordConfirmation" v-model="passwordConfirmation">
+        <p v-if="passwordError">{{ passwordError }}</p>
         <button class="logged-out-button">Reset Password</button>
       </form>
     </div>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
+import { usePasswordValidation } from '../composables/usePasswordValidation';
+
+const { password, passwordConfirmation, passwordError, validatePassword } = usePasswordValidation();
+
+function handleSubmit() {
+  if (validatePassword()) {
+    // Continue with password reset logic
+    // TODO: Password reset logic goes here
+  }
+}
 
 </script>
 <template>
@@ -13,16 +23,19 @@ import NavBarLoggedOut from '../components/NavBarLoggedOut.vue'
           <h3 class="heading-logged-out">Already have an Account? <RouterLink to="/login">Log In</RouterLink>
           </h3>
         </div>
-        <form class="forms" action="register" method="post">
+        <form class="forms" action="register" @submit.prevent="handleSubmit" method="post">
           <label for="firstName">First name</label>
           <input type="text" id="firstName" name="firstName">
           <label for="lastName">Last name</label>
           <input type="text" id="lastName" name="lastName">
           <label for="email">E-Mail</label>
-          <input type="email" id="email" name="email">
+          <input type="email" id="email" name="email" v-model="password">
           <label for="password">Password</label>
           <input type="password" id="password" name="password">
           <p class="sub-password">Use 8 or more characters with a mix of letters, numbers, and symbols</p>
+          <label for="password">Password Confirmation</label>
+          <input type="password" id="passwordConfirmation" name="passwordConfirmation" v-model="passwordConfirmation">
+          <p v-if="passwordError">{{ passwordError }}</p>
           <button class="logged-out-button">Sign Up</button>
         </form>
       </div>
