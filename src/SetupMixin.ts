0690dddef53b7e35f67277e74f9582f58c036cc1
Anton_Mishchuk <amishchuk@vertafore.com>
2023-12-15 22:06:45 +0200

    add return to login if not authenticated

--- src/SetupMixin.ts
+++ src/SetupMixin.ts
@@ -1,4 +1,4 @@
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 import { themeSwitch } from "@/composables/toggleTheme";
 import apiClient from '@/services/apiService';
 
@@ -6,11 +6,16 @@ let isGlobalSetupExecuted = false;
 
 export default {
     created() {
-        const route = useRoute();
         const pagesToIgnore = ['login', 'register', 'registerComplete', 'registerActivate', 'tokenRegenerate',
-                'forgotPassword', 'forgotPasswordEmailSent', 'setNewPassword', 'forgotPasswordSuccess'];
+                        'forgotPassword', 'forgotPasswordEmailSent', 'setNewPassword', 'forgotPasswordSuccess'];
+        const route = useRoute();
+        const router = useRouter();
 
         if (!isGlobalSetupExecuted && route.name) {
+            if (!sessionStorage.getItem('authToken') && !pagesToIgnore.includes(route.name)) {
+                router.push('/login');
+            }
+
             themeSwitch.light=!!sessionStorage.getItem('lightTheme');
             themeSwitch.manipulateClass();
 
